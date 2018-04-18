e9b8458a1163d3b1e32525cc5325b80b4239c916
tt_umv <60070029@it.kmitl.ac.th>
2018-04-19 00:33:20 +0700

    Add logo and some minor change

--- js/markup.js
+++ js/markup.js
@@ -1,16 +1,34 @@
+//refresh debug navbar
 $(document).ready(function(){
-    $('body').scrollspy({target: ".navbar", offset: 50});   
+    $(this).scrollTop(0);
 });
+
+// $(document).ready(function(){
+//   $(".navbar a").on('click', function(event){
+//     if(this.hash !== ""){
+//       event.preventDefault();
+//       var hash = this.hash;
+
+//       $('html, body').animate({
+//         scrollTop:$(hash).offset().top
+//       }, 900, function(){
+//         window.location.hash = hash;
+//       })
+//     }
+//   })
+// })
 
 $(document).ready(function(){
       $(window).scroll(function() {
         if ($(document).scrollTop() > 900) {
           $(".navbar").css("background-color", "#2A2D34");
-          $(".navbar a").css("color", "#E8F1F2")
+          $(".navbar a").css("color", "#E8F1F2");
+          $("#Logo").attr('src', 'pic/logo-white.png');
         } else {
           //welcome page
           $(".navbar").css("background-color", "transparent");
-          $(".navbar a").css("color", "#F8F9FA")
+          $(".navbar a").css("color", "#F8F9FA");
+          $("#Logo").attr('src', 'pic/logo.png');
         }
       });
     });
