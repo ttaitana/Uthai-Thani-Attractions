5bf1818c00dd3cf00ce1233c078fd88ff9885954
tt_umv <60070029@it.kmitl.ac.th>
2018-04-19 21:36:18 +0700

    chage navbar style

--- js/markup.js
+++ js/markup.js
@@ -3,34 +3,26 @@ $(document).ready(function(){
     $(this).scrollTop(0);
 });
 
-// $(document).ready(function(){
-//   $(".navbar a").on('click', function(event){
-//     if(this.hash !== ""){
-//       event.preventDefault();
-//       var hash = this.hash;
-
-//       $('html, body').animate({
-//         scrollTop:$(hash).offset().top
-//       }, 900, function(){
-//         window.location.hash = hash;
-//       })
-//     }
-//   })
-// })
-
 $(document).ready(function(){
       $(window).scroll(function() {
         if ($(document).scrollTop() > 900) {
           $(".navbar").css("background-color", "#2A2D34");
           $(".navbar a").css("color", "#E8F1F2");
           $("#Logo").attr('src', 'pic/logo-white.png');
+          $(".navbar").css("top", "0");
         } else {
           //welcome page
-          $(".navbar").css("background-color", "transparent");
-          $(".navbar a").css("color", "#F8F9FA");
-          $("#Logo").attr('src', 'pic/logo.png');
+          // $(".navbar").css("background-color", "transparent");
+          // $(".navbar a").css("color", "#F8F9FA");
+          // $("#Logo").attr('src', 'pic/logo.png');
+
+          $(".navbar").css("top", "-100px");
         }
       });
     });
+
+$('.navbar a').smoothScroll({
+  autoFocus: true
+});
 
 //click to change element
