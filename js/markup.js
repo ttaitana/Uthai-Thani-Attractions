890ede6e895acc3cb7ff0a52b2a63a5831b69c41
wannowo <60070029@it.kmitl.ac.th>
2018-04-24 19:06:39 +0700

    add card effect and smooth scrolling

--- js/markup.js
+++ js/markup.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 
 $(document).ready(function(){
       $(window).scroll(function() {
-        if ($(document).scrollTop() > 900) {
+        if ($(document).scrollTop() > 800) {
           $(".navbar").css("background-color", "#2A2D34");
           $(".navbar a").css("color", "#E8F1F2");
           $("#Logo").attr('src', 'pic/logo-white.png');
@@ -16,6 +16,30 @@ $(document).ready(function(){
       });
     });
 
+$(document).ready(function(){
+  $("#place1").hover(function(){
+    $("#description h1").css("transform", "translateY(6vh)");
+    $("#description p").css("transform", "translateY(45vh)");
+    $(".att-img img").css("opacity", '.5');
+  },
+  function(){
+    $("#description h1").css("transform", "translateY(-10vh)");
+    $("#description p").css("transform", "translateY(80vh)");
+    $(".att-img img").css("opacity", '1');
+  });
+
+
+  $("#place2").hover(function(){
+    $("#description2 h1").css("transform", "translateY(-74vh)");
+    $("#description2 p").css("transform", "translateY(-35vh)");
+    $(".att-img2 img").css("opacity", '.5');
+  },
+  function(){
+    $("#description2 h1").css("transform", "translateY(-90vh)");
+    $("#description2 p").css("transform", "translateY(-10vh)");
+    $(".att-img2 img").css("opacity", '1');
+  });
+});
 
 //click to flip
 
