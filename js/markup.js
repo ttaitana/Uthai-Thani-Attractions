5d2e49c94da4123a673331c71d556c1695c59a28
tt_umv <60070029@it.kmitl.ac.th>
2018-05-01 16:25:05 +0700

    .......

--- js/markup.js
+++ js/markup.js
@@ -1,6 +1,5 @@
 //navbar hiding
 $(document).ready(function(){
-      $(window).scroll(function() {
         if ($(document).scrollTop() > 700) {
           $(".navbar").css("background-color", "#fff");//#2A2D34
           $(".navbar a").css("color", "#0D1D3A");
@@ -9,9 +8,8 @@ $(document).ready(function(){
         } else {
           $(".navbar").css("top", "-100px");
         }
-      });
+});
 
-    });
 
 //smooth scrolling
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
@@ -22,6 +20,19 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
             behavior: 'smooth'
         });
     });
+});
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+    $(".appear").each(function(){
+      let bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      let bottom_of_window = $(window).scrollTop() + $(window).height();
+      if( bottom_of_window > bottom_of_object ){
+        // $(this).animate({'opacity':'1'},500);
+        $(this).toggle({ effect: "scale", direction: "horizontal" });
+      }
+    });
+  });
 });
 
 $(document).ready(function(){
