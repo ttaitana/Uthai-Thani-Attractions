d647c99b9d66020ebe6a4766f827e0f0601ec75b
wannowo <60070029@it.kmitl.ac.th>
2018-05-02 22:29:57 +0700

    big change lelel

--- js/markup.js
+++ js/markup.js
@@ -1,26 +1,62 @@
 //navbar hiding
 $(document).ready(function(){
-        if ($(document).scrollTop() > 700) {
-          $(".navbar").css("background-color", "#fff");//#2A2D34
+        if ($(document).scrollTop() > 500) {
+          $(".navbar").css("background-color", "#fff");
           $(".navbar a").css("color", "#0D1D3A");
           $("#Logo").attr('src', 'pic/logo.png');
           $(".navbar").css("top", "0");
         } else {
           $(".navbar").css("top", "-100px");
         }
+
+        // Select all links with hashes
+        $('a[href*="#"]')
+          // Remove links that don't actually link to anything
+          .not('[href="#"]')
+          .not('[href="#0"]')
+          .click(function(event) {
+            // On-page links
+            if (
+              location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+              &&
+              location.hostname == this.hostname
+            ) {
+              // Figure out element to scroll to
+              var target = $(this.hash);
+              target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+              // Does a scroll target exist?
+              if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                  scrollTop: target.offset().top
+                }, 800, function() {
+                  // Callback after animation
+                  // Must change focus!
+                  var $target = $(target);
+                  $target.focus();
+                  if ($target.is(":focus")) { // Checking if the target was focused
+                    return false;
+                  } else {
+                    $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                    $target.focus(); // Set focus again
+                  };
+                });
+              }
+            }
+          });
 });
 
 
 //smooth scrolling
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
 
 $(document).ready(function(){
   $(window).scroll(function(){
@@ -28,8 +64,8 @@ $(document).ready(function(){
       let bottom_of_object = $(this).offset().top + $(this).outerHeight();
       let bottom_of_window = $(window).scrollTop() + $(window).height();
       if( bottom_of_window > bottom_of_object ){
-        // $(this).animate({'opacity':'1'},500);
-        $(this).toggle({ effect: "scale", direction: "horizontal" });
+        $(this).animate({'opacity':'1'},500);
+        // $(this).toggle({ effect: "scale", direction: "horizontal" });
       }
     });
   });
--- js/sub-link.js
+++ js/sub-link.js
@@ -4,3 +4,91 @@ $(document).ready(function(){
   $(".parallax3").fadeIn(1000);
   $("#Header-text").animate({top:"-=50px", opacity:1}, "slow");
 });
+
+
+$(document).ready(function(){
+  var i = 1, pos = 0;
+  $("#fst").click(function(){
+    if(i == 1){
+      i = i + 2;
+      pos = pos - (1765*2);
+    }
+    if($("#snd").is(":hidden")){
+      $("#snd").show();
+      $("#fst").text("ดูชั้นก่อนหน้า");
+      $("#snd").text("ดูชั้นต่อไป");
+    }
+    else if(i == 2){
+      $("#fst").text("ดูข้อมูลแต่ละชั้น");
+      $("#snd").hide();
+    }
+    i = i - 1;
+    pos = pos + 1765;
+    $("#struct").css("transform", "translateX(" + pos + "px)");
+  });
+
+  $("#snd").click(function(){
+    if(i == 3){
+      $("#snd").hide();
+      $("#fst").text("ดูชั้นก่อนหน้า");
+    }
+    i = i + 1;
+    pos = pos - 1765;
+    $("#struct").css("transform", "translateX(" + pos + "px)");
+  });
+
+
+  $(function () {
+
+    /* SET PARAMETERS */
+    var change_img_time     = 3500;
+    var transition_speed    = 600;
+
+    var simple_slideshow    = $("#Slider"),
+        listItems           = simple_slideshow.children('li'),
+        listLen             = listItems.length,
+        i                   = 0,
+
+        changeList = function () {
+
+            listItems.eq(i).fadeOut(transition_speed, function () {
+                i += 1;
+                if (i === listLen) {
+                    i = 0;
+                }
+                listItems.eq(i).fadeIn(transition_speed);
+            });
+
+        };
+
+    listItems.not(':first').hide();
+    setInterval(changeList, change_img_time);
+  }); //slide show1
+
+  $(function () {
+
+  /* SET PARAMETERS */
+  var change_img_time     = 3500;
+  var transition_speed    = 600;
+
+  var simple_slideshow    = $("#Slider2"),
+      listItems           = simple_slideshow.children('li'),
+      listLen             = listItems.length,
+      i                   = 0,
+
+      changeList = function () {
+
+          listItems.eq(i).fadeOut(transition_speed, function () {
+              i += 1;
+              if (i === listLen) {
+                  i = 0;
+              }
+              listItems.eq(i).fadeIn(transition_speed);
+          });
+
+      };
+
+  listItems.not(':first').hide();
+  setInterval(changeList, change_img_time);
+});
+});//slide show2
